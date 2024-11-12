88363c7216c02bc0f345d662d0bc1d86eee116a4
hyeongcheol-jeong <105179763+hyeongcheong-jeong@users.noreply.github.com>
2024-11-12 09:55:07 +0900

    eslint prettier 셋

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+const config = {
+  printWidth: 200, //파일의 max-line-length를 설정합니다.
+  singleQuote: true, //큰따옴표 대신 작은 따옴표를 사용함
+  arrowParens: 'avoid', // arrow function parameter가 하나일 경우 괄호 생략
+  semi: false, //문장의 끝에 세미콜론을 붙임 fasle => asi 오류가 발생할 수 있는 줄의 시작 부분에만 세미콜론을 추가
+  tabWidth: 2, //탭 넓이
+}
+
+export default config
