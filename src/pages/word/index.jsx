bde8685c869183efb4ce2c9da35272f04d082011
jhc <wjdgudcjf00@naver.com>
2025-06-25 08:49:31 +0900

    수정

--- src/pages/word/index.jsx
+++ src/pages/word/index.jsx
@@ -3,6 +3,7 @@ import Days from "./ui/days"
 export default function Word() {
   return (
     <>
+      123
       <Days />
     </>
   )
--- src/pages/word/ui/days.jsx
+++ src/pages/word/ui/days.jsx
@@ -1,10 +1,12 @@
 // import { Link } from "react-router-dom"
-
+import useFetch from '../../../shared/hook/useFetch'
+import CreateWord from '../../../feature/word/post'
 export default function Days() {
   // let days = useFetch('http://localhost:8888/days')
-  
   return (
     <>
+      11111111
+      <button type="button" onClick={useFetch}>fetch</button>
       <ul>
         {/* {days?.map(el => {
           return <li key={el.id}>
@@ -12,7 +14,7 @@ export default function Days() {
           </li>
         })} */}
       </ul>
-      {/* <button type="button" onClick={CreateWord}>데이생성</button> */}
+      <button type="button" onClick={CreateWord}>데이생성</button>
     </>
   )
 }
